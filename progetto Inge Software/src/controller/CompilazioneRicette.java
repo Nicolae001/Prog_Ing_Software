0269b980b2e8371dcd642fa538bddefa9fed5274
Nicolae001 <131882891+Nicolae001@users.noreply.github.com>
2023-08-12 17:03:48 +0200

    Delete CompilazioneRicette.java

--- progetto Inge Software/src/controller/CompilazioneRicette.java
+++
@@ -1,8 +0,0 @@
-package controller;
-
-public class CompilazioneRicette {
-	
-	/*Classe per compilare le ricette. Deve avere un attributo identificativo del Utente che sia autorizzato
-	 * a effetuare tale operazione. La compilazione avviene attraverso la creazione di oggetti Ricetta da 
-	 * registrarsi poi nella ListaRicette */
-}
