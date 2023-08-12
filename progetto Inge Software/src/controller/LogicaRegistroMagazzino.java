1e5e72f36658b156f7182705657e106493e42daa
Nicolae001 <131882891+Nicolae001@users.noreply.github.com>
2023-08-12 23:37:27 +0200

    Add files via upload

--- progetto Inge Software/src/controller/LogicaRegistroMagazzino.java
+++ progetto Inge Software/src/controller/LogicaRegistroMagazzino.java
@@ -1,7 +1,5 @@
 package controller;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Set;
+import java.util.*;
 import model.RegistroMagazzino;
 import model.GenereAlimentare;
 import model.Magazziniere;
@@ -36,20 +34,72 @@ public class LogicaRegistroMagazzino {
 		return res;
 		
 	}
+	
+	private Double[] estraiValori(HashMap<GenereAlimentare, Double> arg) {
+		Double[] res;
+		Collection<Double> list=arg.values();
+		res=list.toArray(new Double[list.size()]);
+		return res;
+	}
+	
+	private int getPosizione(GenereAlimentare[] lista, String arg) {
+		int res=0;
+		for(int i=0;i<lista.length;i++) {
+			if(lista[i].getNome().equals(arg))
+				break;
+			res++;
+		}
+		return res;
+	}
+	
 	public void preleva(Magazziniere m, GenereAlimentare alim, Double qta, Causa c) {
-		registro.getGiacenze()
+		GenereAlimentare[] alimenti=estraiChiavi(registro.getGiacenze());
+		Double[] disponibilita=estraiValori(registro.getGiacenze());
+		int pos=getPosizione(alimenti,alim.getNome());
+		disponibilita[pos]=disponibilita[pos]-qta;
+		HashMap<GenereAlimentare, Double> giacenze=new HashMap<GenereAlimentare, Double>();
+		for(int i=0;i<alimenti.length;i++) {
+			giacenze.put(alimenti[i],disponibilita[i]);
+		}
+		registro.setGiacenze(giacenze);
+		annotaPrelievo(m,alim,qta,c);
+		
 	}
 	
 	public void preleva(Magazziniere m, GenereAlimentare[] alim, Double[] qta,Causa c) {
-		//da implementare
+		if(alim.length!=qta.length) {
+			System.out.println("Impossibile procedere alla registrazione prelievo. Le"
+					+ "lunghezze degli argomenti differiscono");
+			return;
+		}
+		
+		for(int i=0;i<alim.length;i++)
+			preleva(m,alim[i],qta[i],c);
+		
 	}
 	
 	public synchronized void aggiungi(Magazziniere m,GenereAlimentare alim, Double qta,Causa c){
-		//da implementare
+		GenereAlimentare[] alimenti=estraiChiavi(registro.getGiacenze());
+		Double[] disponibilita=estraiValori(registro.getGiacenze());
+		int pos=getPosizione(alimenti,alim.getNome());
+		disponibilita[pos]=disponibilita[pos]+qta;
+		HashMap<GenereAlimentare, Double> giacenze=new HashMap<GenereAlimentare, Double>();
+		for(int i=0;i<alimenti.length;i++) {
+			giacenze.put(alimenti[i],disponibilita[i]);
+		}
+		registro.setGiacenze(giacenze);
+		annotaAggiunta(m,alim,qta,c);
 	}
 	
 	public synchronized void aggiungi(Magazziniere m,GenereAlimentare[] alim, Double[] qta, Causa c){
-		//da implementare
+		if(alim.length!=qta.length) {
+			System.out.println("Impossibile procedere alla registrazione aggiunta. Le"
+					+ "lunghezze degli argomenti differiscono");
+			return;
+		}
+		
+		for(int i=0;i<alim.length;i++)
+			preleva(m,alim[i],qta[i],c);
 	}
 	
 	
@@ -63,26 +113,22 @@ public class LogicaRegistroMagazzino {
 		return res;
 	}
 
-	private synchronized void annotaPrelievo(Magazziniere m, GenereAlimentare[] alim, Double[] qta, Causa c) {
+	private synchronized void annotaPrelievo(Magazziniere m, GenereAlimentare alim, Double qta, Causa c) {
 		StringBuilder sb= new StringBuilder();
 		String nome=m.getNome();
 		String causa=c.name();
 		String data=LocalDateTime.now().toString();
-		for(int i=0; i<alim.length;i++) {
-			sb.append(creaLogPrelievo(nome,alim[i].getNome(), alim[i].getUniMisura(), qta[i],causa,data));
-		}
+		sb.append(creaLogPrelievo(nome,alim.getNome(), alim.getUniMisura(), qta,causa,data));
 		log.add(sb.toString());	
 		
 	}
 	
-	private synchronized void annotaAggiunta(Magazziniere m, GenereAlimentare[] alim, Double[] qta, Causa c) {
+	private synchronized void annotaAggiunta(Magazziniere m, GenereAlimentare alim, Double qta, Causa c) {
 		StringBuilder sb= new StringBuilder();
 		String nome=m.getNome();
 		String causa=c.name();
 		String data=LocalDateTime.now().toString();
-		for(int i=0; i<alim.length;i++) {
-			sb.append(creaLogAggiunta(nome,alim[i].getNome(), alim[i].getUniMisura(), qta[i],causa,data));
-		}
+		sb.append(creaLogAggiunta(nome,alim.getNome(), alim.getUniMisura(), qta,causa,data));
 		log.add(sb.toString());	
 		
 	}
